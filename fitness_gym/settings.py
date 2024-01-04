c498b6d1fe08b8f211b594ddda044a1ace3cf1a1
Ngozi89 <>
2024-01-04 11:51:13 +0000

    Added index page content

--- fitness_gym/settings.py
+++ fitness_gym/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j2$*0%fd98r4xd74@8-s*iem0!sbw!s@i7f%=j**urslqt_xl&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ngozi89-fitness-gym-tgdhql35a4.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-ngozi89-fitness-gym-4jzxgag7x5.us2.codeanyapp.com']
 
 
 # Application definition
