62764eaf04c9e4bcd47ce7ec0628dd2675ffa6f9
Ngozi Omenka <ngozi.madu@live.se>
2024-01-13 16:21:22 +0000

    Added Delivery info and loading overlay

--- fitness_gym/settings.py
+++ fitness_gym/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j2$*0%fd98r4xd74@8-s*iem0!sbw!s@i7f%=j**urslqt_xl&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ngozi89-fitnessgym-hlx1r1vqmbj.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-ngozi89-fitnessgym-uw3qrkxx6xj.ws-eu107.gitpod.io']
 
 
 # Application definition
